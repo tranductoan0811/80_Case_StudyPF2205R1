97b389a5b82c0417764070beb03c3a912306eb45
tranductoan <tranductoan0811@gmail.com>
2022-08-06 18:17:19 +0700

    update

---
+++ HRM.js
@@ -0,0 +1,18 @@
+class Human {
+  constructor(id,fname,dateofbirth, gender, add, email, phone) {
+    this.id = id;
+    this.fname = fname;
+    this.dateofbirth = dateofbirth;
+    this.gender = gender;
+    this.add = add;
+    this.email = email;
+    this.phone = phone;
+  }
+
+  
+
+
+
+
+
+}
